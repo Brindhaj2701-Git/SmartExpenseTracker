cb0309684e5bf597d0336ea7309253e9faeeb284
Brindhaj2701-Git <brindhaj2701@gmail.com>
2026-07-24 22:49:47 +0530

    Add Expense class with constructor and methods

--- Expense.java
+++ Expense.java
@@ -1,3 +1,55 @@
 public class Expense {
 
+    // Variables
+    private int transactionId;
+    private String type;
+    private String category;
+    private double amount;
+
+    // Constructor
+    public Expense(int transactionId, String type, String category, double amount) {
+        this.transactionId = transactionId;
+        this.type = type;
+        this.category = category;
+        this.amount = amount;
+    }
+
+    // Getters
+    public int getTransactionId() {
+        return transactionId;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    // Setters
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    // Display Method
+    public void display() {
+        System.out.println("-------------------------------");
+        System.out.println("Transaction ID : " + transactionId);
+        System.out.println("Type           : " + type);
+        System.out.println("Category       : " + category);
+        System.out.println("Amount         : ₹" + amount);
+    }
 }
--- Main.java
+++ Main.java
@@ -4,6 +4,15 @@ public class Main {
 
         System.out.println("Welcome to Smart Expense Tracker");
 
+        Expense expense = new Expense(
+                101,
+                "Expense",
+                "Food",
+                250.00
+        );
+
+        expense.display();
+
     }
 
 }
